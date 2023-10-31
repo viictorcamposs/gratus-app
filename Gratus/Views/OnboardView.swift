2cddfd79ad05ad12e7dac1220e547ccb79a39922
Victor Campos <campos.viictor@gmail.com>
2023-10-30 21:24:00 -0300

    refactor: create custom GAButton view and refactor OnboardView

--- Gratus/Views/OnboardView.swift
+++ Gratus/Views/OnboardView.swift
@@ -3,56 +3,73 @@ import SwiftUI
 struct OnboardView: View {
     @State private var username = String()
     
+    let defaults = UserDefaults.standard
+    
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            LinearGradient(colors: [.black, Color("DarkerGray")],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea()
             
-            withAnimation(.default) {
-                VStack {
-                    Text("Hey there! Welcome to gratus.")
-                        .font(.title2)
-                        .padding(.bottom)
-                    
-                    Text("What's your name?")
-                        .font(.title3)
-                        .bold()
-                        
-                    
-                    VStack(spacing: 0) {
-                        TextField(
-                            "",
-                            text: $username,
-                            prompt: Text("My name is")
-                                .foregroundColor(.gray)
-                        )
-                            .autocorrectionDisabled()
-                            .frame(maxWidth: 300)
-                            .frame(height: 48)
-                        
-                        Divider()
-                            .background(Color.white)
-                            .frame(maxWidth: 320)
+            
+            VStack {
+                Text("Hey there! Welcome to gratus.")
+                    .font(.system(size: 24))
+                    .padding(.bottom)
+                    .foregroundStyle(.white)
+                
+                Text("What's your first name?")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundStyle(.white)
+                
+                
+                UsernameInput(username: $username)
+                
+                Button {
+                    if username == "" {
+                        print("For better experience you must provide your first name")
+                    } else {
+                        print("Hello, \(username). Welcome to gratus! May this be the beginning of a life full of improvements and achievements.")
                     }
-                    .scenePadding(.vertical)
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("Enter")
-                            .frame(width: 100, height: 48)
-                            .background(.gray)
-                            .foregroundStyle(.white)
-                            .cornerRadius(8)
-                            .bold()
-                    }
-                    .scenePadding(.top)
+                } label: {
+                    GAButton(title: "Enter",
+                             bgColor: .gray,
+                             textColor: .white)
                 }
+                .scenePadding(.top)
             }
         }
-        .foregroundStyle(.white)
+    }
+    
+    func addUsernameToUserDefaults() {
+        
     }
 }
 
 #Preview {
     OnboardView()
+}
+
+struct UsernameInput: View {
+    @Binding var username: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            TextField(
+                "",
+                text: $username,
+                prompt: Text("My name is")
+                    .foregroundColor(.gray)
+            )
+            .foregroundStyle(.white)
+            .autocorrectionDisabled()
+            .frame(width: 300, height: 50)
+            
+            Divider()
+                .background(Color.white)
+                .frame(maxWidth: 320)
+        }
+        .scenePadding(.vertical)
+    }
 }
