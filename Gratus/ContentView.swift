00b689a9fab6210238120472602d6bc316c80745
Victor Campos <campos.viictor@gmail.com>
2023-11-06 09:29:15 -0300

    feat: show different screen if user already filled its name

--- Gratus/ContentView.swift
+++ Gratus/ContentView.swift
@@ -1,11 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var manager: GratusManager
+    
     var body: some View {
-        OnboardView()
+        if manager.username != nil {
+            TabView() {
+                CurrentDayGratitudeView()
+                    .environmentObject(manager)
+                    .tabItem {
+                        Image(systemName: "house.fill").padding(.top, 10)
+                    }
+                    
+                ZStack {
+                    BgGradientView()
+                    Text("List of gratitude entries")
+                        .foregroundStyle(.white)
+                }
+                    .tabItem {
+                        Image(systemName: "list.bullet.rectangle.fill")
+                    }
+                    
+            }
+            .accentColor(.white)
+        } else {
+            OnboardView()
+                .environmentObject(manager)
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .environmentObject(GratusManager())
 }
