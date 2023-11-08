2c4c5abb5c04049689b6c50a89668337572f9d95
Victor Campos <campos.viictor@gmail.com>
2023-11-08 19:25:34 -0300

    refactor: remove environment object & add dark theme as default

--- Gratus/GratusApp.swift
+++ Gratus/GratusApp.swift
@@ -2,12 +2,10 @@ import SwiftUI
 
 @main
 struct GratusApp: App {
-    @StateObject var manager = GratusManager()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(manager)
+                .preferredColorScheme(.dark)
         }
     }
 }
