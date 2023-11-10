5647111fbcb3a21488ff98abbb159bbf8fb3d00d
Victor Campos <campos.viictor@gmail.com>
2023-11-10 17:59:54 -0300

    feat: add tab selection state on ContentView

--- Gratus/ContentView.swift
+++ Gratus/ContentView.swift
@@ -1,22 +1,31 @@
 import SwiftUI
 
+enum Tab {
+    case feature
+    case list
+}
+
 struct ContentView: View {
+    
     @EnvironmentObject var manager: DataManager
+    @State private var tab: Tab = .feature
     
     var body: some View {
         if manager.username.isEmpty {
             OnboardView()
         } else {
-            TabView() {
+            TabView(selection: $tab) {
                 CurrentDayGratitudeView()
                     .tabItem {
                         Image(systemName: "house.fill").padding(.top, 10)
                     }
+                    .tag(Tab.feature)
                     
                 GratitudeListView()
                     .tabItem {
                         Image(systemName: "list.bullet.rectangle.fill")
                     }
+                    .tag(Tab.list)
                     
             }
             .accentColor(.white)
